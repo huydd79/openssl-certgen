1f4f3be53d20b10831135f05e0131fffef4deab8
huydd79 <86530496+huydd79@users.noreply.github.com>
2022-11-26 14:20:56 +0700

    Update gen.all.cert.sh

--- gen.all.cert.sh
+++ gen.all.cert.sh
@@ -20,13 +20,17 @@ openssl rsa -passin pass:changeme -in $CERT_DIR/server.pass.key -out $CERT_DIR/s
 openssl req -new -key $CERT_DIR/server.key -out $CERT_DIR/server.csr -config cert-server.conf
 openssl x509 -CAcreateserial -req -days 3650 -in $CERT_DIR/server.csr -CA $CERT_DIR/ca.crt -CAkey $CERT_DIR/ca.key -out $CERT_DIR/server.crt -extensions v3_req -extfile cert-server.conf
 
+echo "Generating importable pfx. Enter password if you want to secure it ..."
+cat $CERT_DIR/server.key $CERT_DIR/server.crt $CERT_DIR/ca.crt > $CERT_DIR/server.pem
+openssl pkcs12 -export -out $CERT_DIR/server.pfx -inkey $CERT_DIR/server.key -in $CERT_DIR/server.pem -certfile $CERT_DIR/ca.crt
+
 echo "Generating Client Certs..."
 openssl genrsa -aes256 -passout pass:changeme -out $CERT_DIR/client.pass.key 4096
 openssl rsa -passin pass:changeme -in $CERT_DIR/client.pass.key -out $CERT_DIR/client.key
 openssl req -new -key $CERT_DIR/client.key -out $CERT_DIR/client.csr -config cert-client.conf
 openssl x509 -req -days 3650 -in $CERT_DIR/client.csr -CA $CERT_DIR/ca.crt -CAkey $CERT_DIR/ca.key -out $CERT_DIR/client.crt -extensions v3_req -extfile cert-client.conf
 
-echo "Generating Client Certs..."
+echo "Generating importable pfx. Enter password if you want to secure it ..."
 cat $CERT_DIR/client.key $CERT_DIR/client.crt $CERT_DIR/ca.crt > $CERT_DIR/client.pem
 openssl pkcs12 -export -out $CERT_DIR/client.pfx -inkey $CERT_DIR/client.key -in $CERT_DIR/client.pem -certfile $CERT_DIR/ca.crt
 set +x
